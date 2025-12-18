92aa336de0c8e513a29daf2017f059757cb3e9b7
georgemathachan <gmathachan44@gmail.com>
2025-12-18 17:38:01 +0000

    optimised q1

--- 1.duplicate-integer.py
+++ 1.duplicate-integer.py
@@ -1,19 +1,28 @@
 ### Contains Duplicate
-### Given an integer array nums, return true if any value 
+### Given an integer array nums, return true if any value
 ### appears more than once in the array, otherwise return false.
-
-
 
 import unittest
 from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        for i in range(0,len(nums)): 
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    return True
-        return False
+        # Original brute-force solution (O(n^2))
+        # for i in range(0, len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
+        # return False
+
+        # Set-based solution
+        # set_nums = set(nums)
+        # if len(nums) == len(set_nums):
+        #     return False
+        # else:
+        #     return True
+
+        # Pythonic concise solution (O(n))
+        return len(nums) != len(set(nums))
 
 
 class TestHasDuplicate(unittest.TestCase):
@@ -28,6 +37,7 @@ class TestHasDuplicate(unittest.TestCase):
     def test_example2(self):
         nums = [1, 2, 3, 4]
         self.assertFalse(self.solution.hasDuplicate(nums))
+
 
 if __name__ == '__main__':
     unittest.main()
